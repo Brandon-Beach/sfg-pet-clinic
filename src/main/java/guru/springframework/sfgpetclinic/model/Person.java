e4dcd715bb65949fe2e26ec763248e8dba05dc1f
Brandon Beach <bbeac29@wgu.edu>
2025-02-07 19:09:44 -0500

    Adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+
+    private String firstName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String lastName;
+}
